38852ffdba9d90ed7cea42927e62b1645564f7e8
vishal gupta <vishalgupta.sf@gmail.com>
2019-09-04 17:28:17 +0530

    update code

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ class UTILS {
     static addNumber(a, b) {
         return a + b;
     }
+
+    static equal(a, b) {
+        return a == b;
+    }
 }
 
 module.exports = UTILS;
--- test/index.test.js
+++ test/index.test.js
@@ -7,4 +7,8 @@ describe("Fake Test", () => {
     it("Should Return 4", () => {
         expect(UTILS.addNumber(2, 2)).equal(4);
     });
+
+    it("Should Return true", () => {
+        expect(UTILS.equal(2, 2)).equal(true);
+    });
 });
