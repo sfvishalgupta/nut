088e87684cccd3e00ee37bb21f85fdfc4665cf72
vishal gupta <vishalgupta.sf@gmail.com>
2019-09-04 16:27:05 +0530

    test commit

---
+++ config.js
@@ -0,0 +1,1 @@
+const API_URL = "http://asdf.com/test/test";
---
+++ test/index.test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const expect = require('chai').expect;
+
+describe("Fake Test", () => {
+    it("Should Return 4", () => {
+        expect(2 + 2).equal(4)
+    });
+});
